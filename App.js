38acbd4c6ffc3db363a781f464a5196fa9631d61
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-03-04 23:27:34 -0300

    feat: add create reminder

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
 
 export default function App() {
+  const [reminder, setReminder] = useState();
+  const [reminders, setReminders] = useState([]);
+
+  const getReminder = (reminder) => {
+    setReminder(reminder);
+  };
+
+  const addReminder = () => {
+    setReminders((reminders) => [reminder, ...reminders]);
+    setReminder('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder='Lembrar...'
+          style={styles.textInput}
+          onChangeText={getReminder}
+          value={reminder}
+        />
+
+        <View style={styles.inputButton}>
+          <Button title='+' onPress={addReminder} />
+        </View>
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        {reminders.map((reminder, key) => (
+          <View style={styles.item}>
+            <Text key={key}>{reminder}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
+  },
+  inputView: { alignItems: 'center', marginBottom: 16 },
+  textInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginBottom: 4,
+    padding: 2,
+    textAlign: 'center',
+  },
+  inputButton: {
+    width: '80%',
+  },
+  item: {
+    width: '80%',
+    padding: 12,
+    backgroundColor: '#CCC',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 8,
+    borderRadius: 8,
+    alignItems: 'center',
   },
 });
